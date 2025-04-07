1f8fb561e38e8a30906502d29abd1d0a33ff2ad2
t0niCoding <antoniosariobaladagmail.com>
2025-04-07 12:32:30 +0200

    Creación modelo y tabla para Movies

--- database/migrations/2025_04_07_092725_create_movies_table.php
+++ database/migrations/2025_04_07_092725_create_movies_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use app\ETL\Entities\Movie\Movie;
 
 return new class extends Migration
 {
@@ -11,8 +12,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('movies', function (Blueprint $table) {
+        Schema::create(Movie::TABLE, function (Blueprint $table) {
             $table->id();
+            $table->string(Movie::ORIGINAL_ID)->unique():
+            $table->string(Movie::TITLE);
+            $table->string(Movie::ORIGINAL_TITLE);
+            $table->json(Movie::GENRES_ID)->nullable();
+            $table->text(Movie::OVERVIEW)->nullable();
+            $table->date(Movie::RELEASE_DATE)->nullable();
+            $table->string(Movie::POSTER_PATH)->nullable();
+            $table->string(Movie::BACKDROP_PATH)->nullable();
+            $table->float(Movie::VOTE_AVERAGE)->nullable();
+            $table->float(Movie::POPULARITY)->nullable();
+           
+            $table->unique(Movie::UNIQUE_KEYS, Movie::TABLE . '_unique');
             $table->timestamps();
         });
     }
@@ -22,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('movies');
+        Schema::dropIfExists(Movie::TABLE);
     }
 };
