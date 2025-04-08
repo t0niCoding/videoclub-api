d16427da8885134962b2696651a305cb43043dc4
t0niCoding <antoniosariobaladagmail.com>
2025-04-08 11:33:26 +0200

    Arreglo sintaxis MovieExtractor

--- app/ETL/Extractors/MovieExtractor.php
+++ app/ETL/Extractors/MovieExtractor.php
@@ -14,27 +14,28 @@ class MovieExtractor
         $page = 1;
 
         $response = $this->byPage($page, $filters);
-        $totalPages = $response['total_pages'] ?? 1
-        
-        for($page = 1; $page <= $totalPages; $page++) {
-            $response = $this byPage($page, $filters);
-            
-            foreach ($response['results'] as $movie) {
+        $totalPages = $response['total_pages'] ?? 1;
+
+        for ($page = 1; $page <= $totalPages; $page++) {
+            $response = $this->byPage($page, $filters);
+
+            foreach ($response['results'] ?? [] as $movie) {
                 yield $movie;
             }
         }
+    }
 
-        public function byPage(int $page, array $filters = []): array
-        {
-            $params = [
-                'sort_by' => 'popularity.desc',
-                'page' => $page,
-                'language' => 'es-ES',
-            ];
+    public function byPage(int $page = 1, array $filters = []): array
+    {
+       
+        $params = [
+            'sort_by' => 'popularity.desc',
+            'page' => $page,
+            'language' => 'es-ES',
+        ];
 
-            $params = array_merge($params, $filters);
+        $params = array_merge($params, $filters);
 
-            return (new TmdbConnection())->getApi()->get($this->endpoint, $params);
-        }
+        return TmdbConnection::get($this->endpoint, $params);
     }
 }
