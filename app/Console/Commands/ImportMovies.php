5a66e3cfef1aa97cefccb8dec8be0da4585fed5b
t0niCoding <antoniosariobaladagmail.com>
2025-04-08 13:00:19 +0200

    Add Command Movies Import

---
+++ app/Console/Commands/ImportMovies.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\ETL\Loaders\MovieLoader;
+use Illuminate\Console\Command;
+
+class ImportMovies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tmdb:movies';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import movies from TMDB';
+
+    /**
+     * Execute the console command.
+     */
+
+     public function __construct(MovieLoader $loader)
+     {
+        parent::__construct();
+        $this->MovieLoader = $loader;
+     }
+    public function handle()
+    {
+        $this->info('Importing movies from TMDB...');
+        $this->MovieLoader->execute();
+    }
+}
